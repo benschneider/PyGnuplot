557d64c00a563d901ed4399c96611c19d7a9ff89
Ben <benh.schneider@gmail.com>
2016-07-28 23:14:50 +0200

    getting ready for PyPI

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 from distutils.core import setup
 setup(name='PyGnuplot',
-      # packages=['PyGnuplot'],
-      py_modules=['PyGnuplot'],
-      version='0.1',
+      packages=['PyGnuplot'],
+      # py_modules=['PyGnuplot'],
+      version='0.2',
       description='Ultra light Python Gnuplot wrapper',
       author='Ben Schneider',
       author_email=' ',
       url='https://github.com/benschneider/PyGnuplot',
       download_url='https://github.com/benschneider/PyGnuplot/tarball/0.1',
-      keywords=['gnuplot', 'plot'])
+      keywords=['gnuplot', 'plot'],
+      classifiers=[],
+      )
