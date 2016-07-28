e8d81bd1ea751efa88d660586c0387ad9f10292b
Ben <benh.schneider@gmail.com>
2016-07-28 22:43:40 +0200

    getting PyPi ready

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from distutils.core import setup
 setup(name='PyGnuplot',
-      version='1.0',
+      packages=['PyGnuplot'],
       py_modules=['PyGnuplot'],
+      version='1.0',
+      description='Ultra light Python Gnuplot wrapper',
       author='Ben Schneider',
-      author_email=' ')
+      author_email=' ',
+      url='https://github.com/benschneider/PyGnuplot',
+      download_url='https://github.com/benschneider/PyGnuplot',
+      keywords=['gnuplot', 'plot'])
