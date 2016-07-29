fe3472ca1920541c45a73593b0e21b2d5329720f
Ben <benh.schneider@gmail.com>
2016-07-29 02:25:17 +0200

    Pip ready

--- example.py
+++ example.py
@@ -6,7 +6,7 @@ y1 = x-25
 y2 = y1*np.sin(x-25)
 
 pg.s([x, y1, y2], filename='example.out')  # save data into a file t.out
-pg.c('set title "example"; set xlabel "x-axis"; set ylabel "y-axis"')
+pg.c('set title "example.pdf"; set xlabel "x-axis"; set ylabel "y-axis"')
 pg.c('set yrange [-25:25]; set key center top')
 pg.c("plot 'example.out' u 1:2 w l t 'y=x-25")  # plot fist part
 pg.c("replot 'example.out' u 1:3 w l t 'y=(x-25)*sin(x-25)'")
