7a51680e924f919b0ffbe03ecffbb399751e9c2f
Ben <benh.schneider@gmail.com>
2017-06-26 00:49:06 +0200

    update-version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='PyGnuplot',
       py_modules=['PyGnuplot'],
-      version='0.9.1',
+      version='0.10.0',
       license='MIT',
       description='Python Gnuplot wrapper',
       author='Ben Schneider',
