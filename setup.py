63c22b09b423f8dfc3b969eefff214fec68b3219
Ben <benh.schneider@gmail.com>
2019-05-30 21:41:44 +0200

    -
    yaml correction

--- setup.py
+++ setup.py
@@ -23,6 +23,5 @@ setup(name='PyGnuplot',
                    "Programming Language :: Python :: 2.7",
                    "Programming Language :: Python :: 3.5",
                    "Programming Language :: Python :: 3.6",
-                   "Programming Language :: Python :: 3.7",
                    "Development Status :: 4 - Beta"],
       )
